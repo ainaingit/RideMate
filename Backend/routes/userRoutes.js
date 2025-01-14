13a0ec074ba3aaac5dad28aee2e37b1a64767a9e
ainaingit <ainakevinramaroson@gmail.com>
2025-01-14 14:26:31 +0300

    Resolving router error
    Still got some problem for connecting the kackend JS and the frontedn HTML , but it work with postman for the backend

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController'); // Importer le contrôleur User
+const userController = require('../controllers/userController');
 
 // Route pour créer un utilisateur
 router.post('/register', userController.createUser);
 
-// Route pour la connexion
+// Route pour se connecter
 router.post('/login', userController.loginUser);
+
+// Vérifier une route de test
+router.get('/test', (req, res) => {
+    res.status(200).json({ message: 'Router is working!' });
+});
 
 module.exports = router;
